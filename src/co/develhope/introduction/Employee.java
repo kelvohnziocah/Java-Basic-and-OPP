a0577dd42119ab32638d9febb32bee89a09bd73a
kelvohnziocah <nziokakelvin02@gmail.com>
2023-07-10 10:39:54 +0300

    Develhope exercises 1 to 5

---
+++ src/co/develhope/introduction/Employee.java
@@ -0,0 +1,33 @@
+package co.develhope.introduction;
+
+public class Employee {
+    private String name;
+    private String position;
+    private double salary;
+    public Employee(String name, String position, double salary) {
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    public class Main{
+        public static void main(String[] args){
+            Employee employee = new Employee("Kelvin", "Junior Java Dev", 200.00;
+            System.out.println("Name:"+employee.getName());
+            System.out.println("Position:"+employee.getPosition());
+            System.out.println("Salary:"+"$"+employee.getSalary());
+
+        }
+}
+}
