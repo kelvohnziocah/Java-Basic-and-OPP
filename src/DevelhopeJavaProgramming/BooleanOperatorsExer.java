7457cb56b9809c13c8542560e0839b5172a36db6
kelvohnziocah <nziokakelvin02@gmail.com>
2023-08-18 15:07:23 +0300

    Boolean Exercise

---
+++ src/DevelhopeJavaProgramming/BooleanOperatorsExer.java
@@ -0,0 +1,10 @@
+package DevelhopeJavaProgramming;
+
+public class BooleanOperatorsExer {
+    public static void main (String[] args){
+        System.out.println( 2 <= 2 && !true);
+        System.out.println( !false && 3 > 2);
+        System.out.println( !(!true || false));
+        System.out.println( 6 > 6 ^ !(true && true));
+    }
+}
